464269654189e734bfc6af0b16e2b629fd512dee
yuuyuu244 <yuuyuu244@yahoo.co.jp>
2018-03-28 23:19:00 +0900

    create new methods

--- deta_structure_and_algorism/c2_1.c
+++ deta_structure_and_algorism/c2_1.c
@@ -5,7 +5,9 @@
 /* 
  * File:   c2_1.c
  * Author: yuki
- *
+ * 
+ * hey guys, i am Java programmer.
+ * C language like Java
  * Created on 2018/03/21, 17:38
  */
 
@@ -29,10 +31,18 @@
 /** number of pop failure */
 #define POP_FAILURE -2
 
-/*
- * peek
- * this method is to confilm the top of stack.
- *
+// args of stack
+int stack[MAX];
+
+// index of stack
+int top;
+
+/**
+ * to confilm the top of stack.
+ * @param stack args of stack
+ * @param top   index of stack
+ * @param data  input data
+ * @return 
  */
 int peek(int stack[], int *top, int *data) {
     if((*top) > 0) {
@@ -44,12 +54,99 @@ int peek(int stack[], int *top, int *data) {
     
 }
 
-
-/*
- * 
+/**
+ * inisialize method.
+ * @param top index of stack
  */
-int main(int argc, char** argv) {
+void stack_init (int *top) {
+    
+   // defalt number 0
+    *top = 0;
+}
 
-    return (EXIT_SUCCESS);
+/**
+ * to display items of stack.
+ * @param stack args of stack
+ * @param top   index of stack
+ */
+void display (int stack[], int top) {
+    
+    printf ("STACK (%d) :", top);
+    
+    for(int i = 0; i < top; i++) {
+        printf("%d ", stack[i]);
+    }
+    
+    printf("\n");
+}
+
+/**
+ * to push into stack.
+ * @param stack args of stack
+ * @param top   index of stack
+ * @param data  input data
+ * @return result value
+ */
+int push(int stack[], int *top, int data) {
+    
+    // stack length exceeds a number of max
+    if (*top >= MAX) {
+        printf("stack length exceeds a number of max");
+        // stack overflow
+        return PUSH_FAILURE;
+    } else {
+
+        // store data to stack
+        stack[*top] = data;
+        (*top)++;
+        return PUSH_SUCCESS;
+    }
+}
+
+/**
+ * to pop out of stack.
+ * @param stack args of stack
+ * @param top   index of stack
+ * @param data  input data
+ * @return result value
+ */
+int pop (int stack[],int *top, int *data) {
+    
+    // stack length below a number of 0
+    if (*top <= 0) {
+        printf("stack length below a number of 0");
+        
+        // stack empty
+        return POP_FAILURE;
+    } else {
+    
+        *data = stack[*top - 1];
+        (*top)--;
+        return POP_SUCCESS;
+    }
+}
+
+/**
+ * main method.
+ */
+int main() {
+
+    // input data
+    //int data;
+    
+    // initialization
+    stack_init(&top);
+    
+    push(stack, &top, 300);
+    
+    display(stack, top);
+
+    push(stack, &top, 200);
+    
+    display(stack, top);
+    
+    printfpop
+    
+    return EXIT_SUCCESS;
 }
 
